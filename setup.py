a017b8b462df742ba9a91de4907664b92313d070
Harry Reeder <harry@harryreeder.co.uk>
2020-02-18 18:55:18 +0000

    🎨 Utilise VERSION file

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
+
+with open("VERSION") as version_file:
+    version = version_file.read().strip()
+
+
 setup(
     name="assh",
-    version="0.2.0",
+    version=version,
     packages=find_packages(),
     install_requires=["boto3~=1.9.85", "botostubs~=0.12", "Click~=7.0", "PyYAML~=5.3",],
     extras_require={
